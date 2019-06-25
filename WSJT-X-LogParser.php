cf1513a536f402076be06c40a0fc1e23f00269d6
Federico Saccà <info@federicosacca.it>
2019-06-25 16:03:05 +0100

    Update WSJT-X-LogParser.php
    Data in Array

--- WSJT-X-LogParser.php
+++ WSJT-X-LogParser.php
@@ -1,31 +1,36 @@
 <?php
-   function log_reader(){
-   $pattern = "/.*CQ.*/m"; 
-   $logfile = preg_grep($pattern, file('/Users/xxxxxx/Library/Application Support/WSJT-X/ALL.txt'));
-   $logfile = preg_replace('/\s+/m', ',', $logfile);
+function log_reader(){
+$pattern = "/.*CQ.*/m"; 
 
-   // iterate through the list.
-   foreach($logfile as $ora_book) {
+$logfile = preg_grep($pattern, file('/Users/homedirectory/Library/Application Support/WSJT-X/ALL.txt'));
 
-   $logfile = rtrim($ora_book, ',');
-
-   $date = substr($logfile, 0, 6 );
-   $day =  substr($date, 0, 2 );
-   $month =  substr($date, 2, 2 );
-   $year =  substr($date, 4, 2 );
+$logfile = preg_replace('/\s+/m', ',', $logfile);
 
 
-   $time = substr($logfile, 7, 6 );
-   $hour = substr($logfile, 7, 2 );
-   $minutes = substr($logfile, 9, 2 );
-   $seconds = substr($logfile, 11, 2 );
+// iterate through the list.
+foreach($logfile as $ora_book) {
 
-   echo $day ."/". $month ."/". $year .",";
-   echo $hour .":". $minutes .":". $seconds ."";
+ $logfile = rtrim($ora_book, ',');
 
-  $logfile = substr($logfile, 13);
-   echo $logfile."\n";
-  }
+ $date = substr($logfile, 0, 6 );
+ $day =  substr($date, 0, 2 );
+ $month =  substr($date, 2, 2 );
+ $year =  substr($date, 4, 2 );
+
+ 
+ $time = substr($logfile, 7, 6 );
+ $hour = substr($logfile, 7, 2 );
+ $minutes = substr($logfile, 9, 2 );
+ $seconds = substr($logfile, 11, 2 );
+
+
+$logfile = $day ."/". $month ."/". $year ."," . $hour .":". $minutes .":". $seconds ."". substr($logfile, 13);
+
+ print_r (explode(",",$logfile));
+}
+
+
+
 }
 
 
@@ -34,6 +39,5 @@ log_reader();
 sleep(15);
 }
 
-//Example of output: 19/06/25,14:54:45,7.074,Rx,FT8,-5,-1.4,1543,CQ,WA0SD,DN84
 
 ?>
